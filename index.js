355453e350b4f41ae6b3daa5435cdba2d187a1b0
Noelle <noelle.maingi@student.moringaschool.com>
2023-06-21 22:25:31 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+function modifyingelement() {
+  let mainNode = document.querySelector("main#main");
+  mainNode.parentNode.removeChild(mainNode);
+
+  let newHeader = document.createElement("h1");
+  newHeader.id = "victory";
+  newHeader.innerHTML = "Noelle Maingi is the champion";
+  document.body.appendChild(newHeader);
+
+  return newHeader;
+}
+
+modifyingelement();
